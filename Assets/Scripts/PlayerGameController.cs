f8cd66a78d68a72a6e55f11d748d5f858ff1390e
bilalerdemm <bilalerdemm96@gmail.com>
2023-01-03 17:08:29 +0300

    Mantıksal bazı sıkıntılar var düzenlemeye gidilior

--- Assets/Scripts/PlayerGameController.cs
+++ Assets/Scripts/PlayerGameController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerGameController : MonoBehaviour
 {
     public List<GameObject> Cubes = new List<GameObject>();
     public Transform spawnPoint;
     public int score = 0;
+    public GameObject losePanel, winPanel;
+    public Text scoreText;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -21,17 +24,29 @@ public class PlayerGameController : MonoBehaviour
         }
         if (other.gameObject.CompareTag("LowObstacle") && Cubes.Count > 0)
         {
-            score--;
+            score = score - 1;
+            if (score < 0)
+            {
+                losePanel.SetActive(true);
+            }
             Debug.Log(Cubes.Count);
             Debug.Log("LowObstacle");
-            Destroy(Cubes[Cubes.Count - 1].gameObject);
-            Cubes.RemoveAt(Cubes.Count - 1);
-        }
-        else
-        {
-            Debug.Log("Fail");
+            if (Cubes.Count > 0)
+            {
+                Destroy(Cubes[Cubes.Count - 1].gameObject);
+                Cubes.RemoveAt(Cubes.Count - 1);
+
+            }
+
         }
 
+        else if (Cubes.Count <= 0)
+        {
+            Debug.Log("Fail");
+            //losePanel.SetActive(true);
+        }
+
+        
         if (other.gameObject.CompareTag("HiObstacle") && Cubes.Count > 0)
         {
             score = 0;
@@ -43,10 +58,12 @@ public class PlayerGameController : MonoBehaviour
             }
             Cubes.Clear();
         }
+        /*
         else
         {
             Debug.Log("Fail");
+            //losePanel.SetActive(true);
         }
-
+        */
     }
 }
